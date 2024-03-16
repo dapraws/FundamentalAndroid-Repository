0e117c298c7e90d5afeba47d2d58a906ad313f01
dapraws <darrelprawira26@gmail.com>
2024-03-16 16:36:02 +0700

    update

--- app/src/main/java/com/example/mysettingpreference/MyPreferenceFragment.kt
+++ app/src/main/java/com/example/mysettingpreference/MyPreferenceFragment.kt
@@ -5,9 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.CheckBoxPreference
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class MyPreferenceFragment : PreferenceFragmentCompat() {
+    private lateinit var NAME: String
+    private lateinit var EMAIL: String
+    private lateinit var AGE: String
+    private lateinit var PHONE: String
+    private lateinit var LOVE: String
+    private lateinit var namePreference: EditTextPreference
+    private lateinit var emailPreference: EditTextPreference
+    private lateinit var agePreference: EditTextPreference
+    private lateinit var phonePreference: EditTextPreference
+    private lateinit var isLoveMuPreference: CheckBoxPreference
+    companion object {
+        private const val DEFAULT_VALUE = "Tidak Ada"
+    }
 
     override fun onCreatePreferences(bundle: Bundle?, s: String?) {
         addPreferencesFromResource(R.xml.preferences)
