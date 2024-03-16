13575164ee67ed783d0e93b27a80fb09dd71085c
dapraws <darrelprawira26@gmail.com>
2024-03-16 16:34:27 +0700

    add new fragment

---
+++ app/src/main/java/com/example/mysettingpreference/MyPreferenceFragment.kt
@@ -0,0 +1,14 @@
+package com.example.mysettingpreference
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MyPreferenceFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(bundle: Bundle?, s: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+}
