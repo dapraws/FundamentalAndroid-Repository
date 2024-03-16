47e30e4310b3c89af6c5854af40e0f9a31462e09
dapraws <darrelprawira26@gmail.com>
2024-03-16 16:34:14 +0700

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation ("androidx.preference:preference:1.1.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
